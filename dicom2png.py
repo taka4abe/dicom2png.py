a5e35a334b4e51a451e8b01da9bb7dc782ff8214
Takashi <taka4abe@gmail.com>
2017-10-20 18:28:31 -0700

    Update dicom2png.py

--- dicom2png.py
+++ dicom2png.py
@@ -24,7 +24,7 @@ parser.add_argument('-o', '-128', action='store_true',
                     help='save with 128x128 (One two eight) imaging matrix')
 parser.add_argument('-f', '-512', action='store_true',
                     help='save with 512x512 (Five one two) imaging matrix')
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.11')
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.2')
 args = parser.parse_args()
 
 start = time.time()
@@ -47,7 +47,8 @@ elif args.f:
 
 print('changing dicom to data type:', data_type)
 n, total, sequence_list = 0, 0, []
-target_dir = '.' # defaul target directory
+target_dir = 'renamed' # defaul target directory
+new_dir = data_type[1]
 
 for root, dirs, files in os.walk(target_dir):
     for file_name in files:
@@ -64,21 +65,30 @@ print('total of {} dicom files'.format(total))
 #import sys
 #sys.exit()
 
-if os.path.exists('new_dir'):
-    print("Error!!   Directory 'new_dir' has already existed.")
-    print("Program finished")
-else:
-    pass
+#if os.path.exists(new_dir):
+#    print("Error!!   Directory new_dir has already existed.")
+#    print("Program finished")
+#else:
+#    pass
 
-shutil.copytree(target_dir, 'new_dir')
+if os.path.exists(new_dir):
+    pass
+else:
+    os.mkdir(new_dir)
 check_point = time.time()
 
-for root, dirs, files in os.walk('new_dir'):
+for root, dirs, files in os.walk(target_dir):
     for file_name in files:
         try:
             file_name_, ext = os.path.splitext(file_name)
             if ext == '.dcm':
-                file_path = root + "/" + file_name
+                root1, root2 = root.split("/", 1)
+                save_root = "new_dir/" + root2 # file path to save png/jpg file
+                if os.path.exists(save_root):
+                    pass
+                else:
+                    os.makedirs(save_root)
+                file_path = root + "/" + file_name # file path to read dicom file
                 try:
                     ds = dicom.read_file(file_path)
                     pix = ds.pixel_array
@@ -87,31 +97,42 @@ for root, dirs, files in os.walk('new_dir'):
                     except:
                         pass
                     if data_type[0] == '16-bit':
-                        pix = (((pix - np.min(pix))/ (np.max(pix) - np.min(pix))
+                        if np.max(pix) - np.min(pix) != 0:
+                            pix = (((pix - np.min(pix))/ (np.max(pix) - np.min(pix))
                                ) * 65535).astype(np.uint16)
+                        else:
+                            pass
                         pix = pix.astype(np.uint16)
-                        save_name = root + "/" + file_name_ + ".png"
-                        Image.fromarray(np.uint16(pix)).convert('L').resize(
+                        save_name = save_root + "/" + file_name_ + ".png"
+                        if os.path.exists(save_name):
+                            break
+                        else:
+                            Image.fromarray(np.uint16(pix)).convert('L').resize(
                             (reso, reso), Image.LANCZOS).save(save_name)
                     else: #data_type[0] == '8-bit'
-                        pix = (((pix - np.min(pix))/ (np.max(pix)-np.min(pix))
+                        if np.max(pix) - np.min(pix) != 0:
+                            pix = (((pix - np.min(pix))/ (np.max(pix)-np.min(pix))
                                ) * 255).astype(np.uint8)
+                        else:
+                            pass
                         if data_type[1] == 'jpg':
-                            save_name = root + "/" + file_name_ + ".jpg"
+                            save_name = save_root + "/" + file_name_ + ".jpg"
                         else: #elif data_type[1] == 'png':
-                            save_name = root + "/" + file_name_ + ".png"
-                        if data_type[2] == 'gray_scale':
-                            Image.fromarray(pix).convert('L').resize(
-                                (reso, reso), Image.LANCZOS).save(save_name)
-                        else: #elif data_type[2] == 'RGB':
-                            tmp_reso = [pix.shape[0], pix.shape[1]]
-                            pix = np.reshape(pix, (tmp_reso[0]*tmp_reso[1], 1))
-                            pix2 = np.append(pix, pix, axis = 1)
-                            pix = np.append(pix2, pix, axis = 1)
-                            pix = np.reshape(pix, (tmp_reso[0], tmp_reso[1], 3))
-                            Image.fromarray(np.uint8(pix)).convert('L').resize(
-                                (reso, reso), Image.LANCZOS).save(save_name)
-                    os.remove(file_path)
+                            save_name = save_root + "/" + file_name_ + ".png"
+                        if os.path.exists(save_name):
+                            break
+                        else:
+                            if data_type[2] == 'gray_scale':
+                                Image.fromarray(pix).convert('L').resize(
+                                    (reso, reso), Image.LANCZOS).save(save_name)
+                            else: #elif data_type[2] == 'RGB':
+                                tmp_reso = [pix.shape[0], pix.shape[1]]
+                                pix = np.reshape(pix, (tmp_reso[0]*tmp_reso[1], 1))
+                                pix2 = np.append(pix, pix, axis = 1)
+                                pix = np.append(pix2, pix, axis = 1)
+                                pix = np.reshape(pix, (tmp_reso[0], tmp_reso[1], 3))
+                                Image.fromarray(np.uint8(pix)).convert('L').resize(
+                                    (reso, reso), Image.LANCZOS).save(save_name)
                 except:
                     pass
             else:
@@ -120,12 +141,11 @@ for root, dirs, files in os.walk('new_dir'):
             pass
 
         n += 1
-        if n == 10 or n == 20 or n == 50 or n% 100 == 0:
+        verpose_point = total // 100
+        if n == 10 or n == 20 or n == 50 or n == 100 or n % verpose_point == 0:
             elapsed_time = time.time() - check_point
-            print("{0} / {1} cases processed,  elapsed time: {2:2.02f} sec".format(n, total, elapsed_time))
-            print("{0:2.02f} sec to finish converting".format(((elapsed_time/n)*total)-elapsed_time))
-            print(' ')
+            print("{0}/{1} processed,  elapsed/est_total: {2:2.0f}/{3:2.0f} sec".format(n, total, elapsed_time, ((elapsed_time/n)*total)))
 
 elapsed_time = time.time() - start
-print("{0} cases processed with {1} sec.".format(n, elapsed_time))
+print("{0} cases processed with {1:2.0f} sec.".format(n, elapsed_time))
 print("Finished!!")
